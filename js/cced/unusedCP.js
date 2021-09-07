4e2b80fdf8a3c6c38d084b7a0ae713c980eccf7f
Paradragon <11151153+Paradragon@users.noreply.github.com>
2021-09-07 23:59:50 +0300

    Typo fix

--- js/cced/unusedCP.js
+++ js/cced/unusedCP.js
@@ -11,7 +11,7 @@ function getUnusedSkillPoints(playerModel) {
 
 sc.PlayerModel.inject({
     onVarAccess(a, b) {
-        // We need to run out code before parent() to avoid 
+        // We need to run our code before parent() to avoid 
         // an "Unsupported var access" error that will be thrown by parent()
         if (b[0] == "player") {
             switch (b[1]) {
